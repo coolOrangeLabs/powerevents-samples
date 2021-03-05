b82ed498ed1f595ff21abf085313629c1d920a77
Christian Gessner <christian@gessner.cc>
2021-03-05 10:55:39 +0100

    Added Event

---
+++ Restrict Item state change when position number empty/Events/Sample.RestrictItemStateChange.ps1
@@ -0,0 +1,20 @@
+﻿#=============================================================================#
+# PowerShell script sample for COOLORANGE powerEvents                         #
+# Copyright (c) COOLORANGE s.r.l. - All rights reserved.                      #
+#                                                                             #
+# THIS SCRIPT/CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER   #
+# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  #
+#=============================================================================#
+
+Register-VaultEvent -EventName UpdateItemStates_Restrictions -Action 'RestrictUpdateItemStates'
+function RestrictUpdateItemStates($items) {
+	foreach ($item in $items) {
+		$bomRows = Get-VaultItemBom -Number $item._Number
+		foreach ($bomRow in $bomRows) {
+			if (-not $bomRow.Bom_PositionNumber) {
+				Add-VaultRestriction -EntityName "$($item._Number): $($bomRow.Bom_Number)" -Message "Position of row '$($bomRow.Bom_Number)' is empty!"
+			}
+		}
+	}
+}
---
+++ Restrict Item state change when position number empty/README.md
@@ -0,0 +1,12 @@
+# Restrict Item state change when position number empty
+
+## Event Description
+Restricts the state transition of items when position numbers are missing in the BOM
+
+## Installation
+Download all files (Events and Modules) from the current subdirectory of this repository to your powerEvents directory: *C:\ProgramData\coolOrange\powerEvents*
+
+## At your own risk
+The usage of these samples is at your own risk. There is no free support related to the samples. However, if you have questions to powerEvents, then visit http://www.coolorange.com/wiki or start a conversation in our support forum at http://support.coolorange.com/support/discussions
+
+![coolOrange](https://user-images.githubusercontent.com/36075173/46519882-4b518880-c87a-11e8-8dab-dffe826a9630.png)
